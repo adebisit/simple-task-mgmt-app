7e80b100a451c2dd9e704b81489a0f25c883ecec
Deolamma <adebanjoemmanuel01@gmail.com>
2024-07-04 18:38:29 +0100

    Created search component

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-function Search() {
-  return <div>Search</div>;
-}
+//importing files...
+import { SEARCH_PLACEHOLDER_TEXT } from "../constants.js";
+/*
+ * Search: A component that aids user to search for a presaved task
+ */
+const Search = () => {
+  return (
+    <>
+      <input
+        id="search"
+        type="text"
+        placeholder={SEARCH_PLACEHOLDER_TEXT}
+        className="border border-gray-400 rounded-lg p-2 w-full"
+      />
+    </>
+  );
+};
 
 export default Search;
+
