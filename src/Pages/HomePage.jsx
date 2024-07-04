8bb0126da894b5a029e9705e78a2e13aa9d8960b
Deolamma <adebanjoemmanuel01@gmail.com>
2024-07-04 18:42:39 +0100

    Homepage of the application

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
+//importing files...
 import Navbar from "../components/Navbar";
+import Search from "../components/Search";
+import TaskCard from "../components/TaskCard";
 
 function HomePage() {
   return (
-    <div>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center pt-16 pl-24 pr-24">
       <Navbar />
+      <Search />
+      <TaskCard />
     </div>
   );
 }
