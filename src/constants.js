b10699f20265ce47708bb9071709e7a50a51801d
Deolamma <adebanjoemmanuel01@gmail.com>
2024-07-04 18:43:29 +0100

    Contains static texts

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const HEADER_TEXT="TASK MANAGER";
+export const SEARCH_PLACEHOLDER_TEXT="Search for a task";
+export const TASK_TITLE="Task 1";
+export const TASK_DESCRIPTION="Short description about task";
