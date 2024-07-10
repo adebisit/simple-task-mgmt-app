dc03c7f0bb38dda5b7c08d2fefe5982c529099de
Deolamma <adebanjoemmanuel01@gmail.com>
2024-07-10 12:06:32 +0100

    Adding the sorting and filtering property to search component

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,22 +1,27 @@
+//importing react packages...
 import React from "react";
 
 //importing files...
+import { FilterIcon, SortIcon } from "./Icon.jsx";
 import { SEARCH_PLACEHOLDER_TEXT } from "../constants.js";
-/*
- * Search: A component that aids user to search for a presaved task
- */
+
 const Search = () => {
   return (
-    <>
-      <input
-        id="search"
-        type="text"
-        placeholder={SEARCH_PLACEHOLDER_TEXT}
-        className="border border-gray-400 rounded-lg p-2 w-full"
-      />
-    </>
+    <div className="flex items-center w-full mb-8">
+      <div className="flex-grow">
+        <input
+          id="search"
+          type="text"
+          placeholder={SEARCH_PLACEHOLDER_TEXT}
+          className="border border-2 border-gray-300 rounded-lg p-2 w-full"
+        />
+      </div>
+      <div className="flex justify-evenly w-auto ml-4 space-x-2">
+        <FilterIcon />
+        <SortIcon />
+      </div>
+    </div>
   );
 };
 
 export default Search;
-
