b6f1bd86b25cb5e8f17c2747dd0752044a3264cf
Deolamma <adebanjoemmanuel01@gmail.com>
2024-07-10 12:05:46 +0100

    removed font-inter styling property

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,11 @@
+//importing react packages...
 import React from "react";
 
 //importing files...
 import { HEADER_TEXT } from "../constants";
 
 const Navbar = () => {
-  return (
-    <h1 className="text-3xl font-bold mb-4 text-gray-500 font-roboto">
-      {HEADER_TEXT}
-    </h1>
-  );
+  return <h1 className="text-3xl mb-4 text-gray-500">{HEADER_TEXT}</h1>;
 };
 
 export default Navbar;
