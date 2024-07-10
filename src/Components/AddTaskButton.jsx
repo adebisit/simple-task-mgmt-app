f60e2fca7e2aca6db10e32cbb6979e477cf35c7b
Deolamma <adebanjoemmanuel01@gmail.com>
2024-07-10 12:03:43 +0100

    Button to create a new task

---
+++ src/Components/AddTaskButton.jsx
@@ -0,0 +1,15 @@
+//importing react packages...
+import React from "react";
+
+//importing files...
+import { PlusIcon } from "./Icon";
+
+const AddTaskButton = () => {
+  return (
+    <button className="fixed bottom-12 right-4 md:right-12 bg-blue-500 lg:right-28 text-white rounded-full p-4 shadow-lg hover:bg-blue-600 focus:outline-none items-center justify-center">
+      <PlusIcon />
+    </button>
+  );
+};
+
+export default AddTaskButton;
