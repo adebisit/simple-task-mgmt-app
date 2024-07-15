d50c481dfa6a474a839c9b5c4350ed37f8198947
Deolamma <adebanjoemmanuel01@gmail.com>
2024-07-15 16:44:21 +0100

    Refactored HomePage removing over-abstraction of components

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,20 +2,22 @@
 import React from "react";
 
 //importing components...
-import Navbar from "../components/Navbar";
 import Search from "../components/Search";
 import TaskCard from "../components/TaskCard";
 import AddTaskButton from "../components/AddTaskButton";
-import SeeMoreLink from "../components/SeeMoreLink";
 
 function HomePage() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center pt-16 px-4 sm:px-8 md:px-32 lg:px-64 xl:px-64">
-      <Navbar />
+      <h1 className="text-3xl mb-4 text-gray-500">Task Manager</h1>
       <Search />
-      <TaskCard />
+      <TaskCard/>
+      <div>
+        <button className="text-blue-500 underline font-bold">
+          <span>See More...</span>
+        </button>
+      </div>
       <AddTaskButton />
-      <SeeMoreLink/>
     </div>
   );
 }
