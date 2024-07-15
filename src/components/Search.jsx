4723593a3b4e38e916e7c8d5f3006df8954e3214
preciousyaks17 <johndoe@example.com>
2024-07-15 12:46:33 +0100

    changed the icons to svgs

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 
 //importing files...
-import { FilterIcon, SortIcon } from "./Icon.jsx";
 import { SEARCH_PLACEHOLDER_TEXT } from "../constants.js";
 
 const Search = () => {
@@ -17,8 +16,47 @@ const Search = () => {
         />
       </div>
       <div className="flex justify-evenly w-auto ml-4 space-x-2">
-        <FilterIcon />
-        <SortIcon />
+        {/* Filter  */}
+        <button>
+          <svg
+            width="35"
+            height="35"
+            viewBox="0 0 35 35"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="35" height="35" rx="10" fill="#D9D9D9" />
+            <path
+              d="M25 11H10L16 18.095V23L19 24.5V18.095L25 11Z"
+              stroke="#101828"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </button>
+        {/* Sort */}
+        <button>
+          <svg
+            width="35"
+            height="35"
+            viewBox="0 0 35 35"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="35" height="35" rx="10" fill="#D9D9D9" />
+            <path
+              d="M13 17.8333L18 22L23 17.8333M13 12L18 16.1667L23 12"
+              stroke="#101828"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </button>
+
+        {/* <FilterIcon /> */}
+        {/* <SortIcon /> */}
       </div>
     </div>
   );
