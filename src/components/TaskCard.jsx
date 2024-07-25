47547a4665a35f7b913babc6b4707c051fb712d1
Emmanuel Adebanjo <adebanjoemmanuel01@gmail.com>
2024-07-25 13:05:09 +0100

    Removed constant texts

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
 import moment from "moment";
 import Moment from "react-moment";
 import "../index";
 //importing files...
-import { TASK_DESCRIPTION, TASK_TITLE } from "../constants";
 import { DeleteIcon, EditIcon } from "./Icon";
 import CustomRadioButton from "./shared/CustomRadioButton";
 
@@ -14,16 +12,16 @@ const TaskCard = () => {
   return (
     <>
       <div className="flex flex-row items-start mt-4 mb-4 w-full items-center">
-        <CustomRadioButton isIconChecked />
+        <CustomRadioButton />
         <div className="flex-1">
           <div style={{ fontSize: "20px" }} className="font-bold">
-            {TASK_TITLE}
+            Task 1
           </div>
           <div
             style={{ fontSize: "16px" }}
             className="text-gray-600 font-inter"
           >
-            {TASK_DESCRIPTION}
+            Short description about task
           </div>
           <div style={{ fontSize: "12px" }} className="font-bold text-red-600">
             Due: <Moment toNow>{dueDate}</Moment>
