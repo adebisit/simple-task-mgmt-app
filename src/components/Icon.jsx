8f67d40abc6db02999d1cd7eb7e343f31f8e90c4
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-07-28 13:24:30 +0100

    Added props to the CloseIcon component

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -32,9 +32,9 @@ const SortIcon = () => {
     </div>
   );
 };
-const CloseIcon = () => {
+const CloseIcon = (props) => {
   return (
-      <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
         <path d="M13 1L1 13M1 1L13 13" stroke="#101828" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
       </svg>
   );
