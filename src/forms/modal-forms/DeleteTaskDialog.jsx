7bd4f247a5ace212b6481e66e88152638c7f02e0
Emmanuel Adebanjo <adebanjoemmanuel01@gmail.com>
2024-08-11 20:20:49 +0100

    Updated DeleteTaskDialog with logic to delete a task

--- src/forms/modal-forms/DeleteTaskDialog.jsx
+++ src/forms/modal-forms/DeleteTaskDialog.jsx
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
-
 function DeleteTaskDialog() {
-  const [isVisible, setIsVisible] = useState(false);
   return (
     <>
       <div>
-        <div className="title">Delete Task</div>
-        <div className="body">
-          <p>
-            Are you sure you want to delete this task? This action cannot be
-            undone.
-          </p>
-        </div>
-        <div className="footer">
-          <button className="primary">Cancel</button>
-          <button className="secondary">Delete</button>
-        </div>
+        <p>
+          Are you sure you want to delete this task? This action cannot be
+          undone.
+        </p>
       </div>
     </>
   );
