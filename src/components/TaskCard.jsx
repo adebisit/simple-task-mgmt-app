a9bdf1601fdadf76b6b2591d6ed735e7db32b961
Emmanuel Adebanjo <adebanjoemmanuel01@gmail.com>
2024-08-11 20:23:02 +0100

    Updated TaskCard to show details of specific task and show delete dialog on click of delete button

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -2,19 +2,37 @@ import React, { useState } from "react";
 import moment from "moment";
 import Moment from "react-moment";
 
-//importing files...
 import { TASK_DESCRIPTION, TASK_TITLE } from "../constants";
 import { EditIcon, DeleteIcon, CustomRadioButton } from "./Icon";
+import DeleteTaskDialog from "../forms/modal-forms/DeleteTaskDialog";
+import { useModal } from "../hooks/useModal";
+import { deleteTask } from "../services/taskService";
 
-const TaskCard = () => {
+const TaskCard = ({ task }) => {
   const [isIconChecked, setIsIconChecked] = useState(false);
+
+  const { modal: DeleteTaskDialogModal, setIsOpen } = useModal({
+    title: "Delete A Task",
+    contentFn: () => <DeleteTaskDialog />,
+    primaryBtnTxt: "Delete",
+    secondaryBtnTxt: "Cancel",
+    loadingComp: "Deleting...",
+    onSave: async () => {
+      await deleteTask("4e9f20d9-ba4d-4170-a241-9ada2ec2f83e");
+    },
+    onCancel: () => {},
+  });
+
+  const openDeleteTaskDialog = () => {
+    setIsOpen(true);
+  };
 
   const handleCheckIconClick = () => {
     setIsIconChecked(!isIconChecked);
   };
 
   //get date from moment package
-  const dueDate = moment().add(5, "days");
+  //const dueDate = moment().add(5, "days");
 
   return (
     <>
@@ -32,26 +50,26 @@ const TaskCard = () => {
         </div>
         <div className="flex-1">
           <div style={{ fontSize: "20px" }} className="font-bold">
-            {TASK_TITLE}
+            {task.name}
           </div>
           <div
             style={{ fontSize: "16px" }}
             className="text-gray-600 font-inter"
           >
-            {TASK_DESCRIPTION}
+            {task.description}
           </div>
-          <div
-            style={{ fontSize: "12px" }}
-            className="font-bold text-red-600"
-          >
-            Due: <Moment toNow>{dueDate}</Moment>
+          <div style={{ fontSize: "12px" }} className="font-bold text-red-600">
+            Due: <Moment toNow>{task.dueDate}</Moment>
           </div>
         </div>
         <div className="flex gap-4">
           <EditIcon />
-          <DeleteIcon />
+          <button onClick={openDeleteTaskDialog}>
+            <DeleteIcon />
+          </button>
         </div>
       </div>
+      {DeleteTaskDialogModal}
     </>
   );
 };
