49056ed9cbcf4f1dbaad2800de51f6afbf78059a
Taiwo Adebisi <adebisijosephh@gmail.com>
2024-09-08 21:52:27 +0100

    remove data.json temporarily & modify svg icons props

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -13,9 +13,9 @@ const EditIcon = ({ ...props }) => {
       <path
         d="M11 4.83333H4C3.46957 4.83333 2.96086 5.08795 2.58579 5.54116C2.21071 5.99437 2 6.60906 2 7.25V24.1667C2 24.8076 2.21071 25.4223 2.58579 25.8755C2.96086 26.3287 3.46957 26.5833 4 26.5833H18C18.5304 26.5833 19.0391 26.3287 19.4142 25.8755C19.7893 25.4223 20 24.8076 20 24.1667V15.7083M18.5 3.02084C18.8978 2.54013 19.4374 2.27007 20 2.27007C20.5626 2.27007 21.1022 2.54013 21.5 3.02084C21.8978 3.50154 22.1213 4.15352 22.1213 4.83333C22.1213 5.51315 21.8978 6.16513 21.5 6.64584L12 18.125L8 19.3333L9 14.5L18.5 3.02084Z"
         stroke="#101828"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
@@ -34,9 +34,9 @@ const DeleteIcon = ({ ...props }) => {
       <path
         d="M3 6H5M5 6H21M5 6V20C5 20.5304 5.21071 21.0391 5.58579 21.4142C5.96086 21.7893 6.46957 22 7 22H17C17.5304 22 18.0391 21.7893 18.4142 21.4142C18.7893 21.0391 19 20.5304 19 20V6H5ZM8 6V4C8 3.46957 8.21071 2.96086 8.58579 2.58579C8.96086 2.21071 9.46957 2 10 2H14C14.5304 2 15.0391 2.21071 15.4142 2.58579C15.7893 2.96086 16 3.46957 16 4V6M10 11V17M14 11V17"
         stroke="#CE001C"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
@@ -53,8 +53,8 @@ const PlusIcon = ({ ...props }) => {
       {...props}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M25 9.375C25.8629 9.375 26.5625 10.0746 26.5625 10.9375V23.4375H39.0625C39.9254 23.4375 40.625 24.1371 40.625 25C40.625 25.8629 39.9254 26.5625 39.0625 26.5625H26.5625V39.0625C26.5625 39.9254 25.8629 40.625 25 40.625C24.1371 40.625 23.4375 39.9254 23.4375 39.0625V26.5625H10.9375C10.0746 26.5625 9.375 25.8629 9.375 25C9.375 24.1371 10.0746 23.4375 10.9375 23.4375H23.4375V10.9375C23.4375 10.0746 24.1371 9.375 25 9.375Z"
         fill="white"
       />
@@ -76,9 +76,9 @@ const FilterIcon = ({ ...props }) => {
       <path
         d="M25 11H10L16 18.095V23L19 24.5V18.095L25 11Z"
         stroke="#101828"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
@@ -98,9 +98,9 @@ const SortIcon = ({ ...props }) => {
       <path
         d="M13 17.8333L18 22L23 17.8333M13 12L18 16.1667L23 12"
         stroke="#101828"
-        stroke-width="2"
-        stroke-Linecap="round"
-        stroke-Linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
@@ -163,9 +163,9 @@ const BackIcon = ({ ...props }) => {
       <path
         d="M20.6236 14.8343H10.009M10.009 14.8343L15.3163 20.0263M10.009 14.8343L15.3163 9.64227"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
